bdcec36ab9484f4bf853dc9f4e063ce73d7dd5e6
Jadhav-Krishna <krishnajadhav85017@gmail.com>
2025-11-16 13:01:49 +0530

    Create Page Object Model

---
+++ src/main/java/com/saucedemo/pages/ProductsPage.java
@@ -0,0 +1,12 @@
+package com.saucedemo.pages;
+
+import org.openqa.selenium.By;
+
+public class ProductsPage extends BasePage {
+    private By productHeader = By.xpath("//span[text()='Products']");
+
+    public boolean isProductheaderDisplyed(){
+        return find(productHeader).isDisplayed();
+    }
+
+}
---
+++ src/test/java/part2/com/saucedemo/base/BaseTest.java
@@ -0,0 +1,9 @@
+package part2.com.saucedemo.base;
+
+import org.openqa.selenium.WebDriver;
+import com.saucedemo.pages.BasePage;
+
+public class BaseTest {
+    protected WebDriver driver;
+    protected BasePage basePage;
+}
